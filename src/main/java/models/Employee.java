8c46f47aa686b6933a29cd55ab08c90cf286c356
ikuto.kaneko <angler1910ki@gmail.com>
2022-08-06 16:42:10 +0900

    Employeeモデル、EmployeeViewモデル作成

---
+++ src/main/java/models/Employee.java
@@ -0,0 +1,104 @@
+package models;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import constants.JpaConst;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 従業員データのDTOモデル
+ *
+ */
+@Table(name = JpaConst.TABLE_EMP)
+@NamedQueries({
+    @NamedQuery(
+//            全ての従業員情報を取得
+            name = JpaConst.Q_EMP_GET_ALL,
+            query = JpaConst.Q_EMP_GET_ALL_DEF),
+    @NamedQuery(
+//            従業員情報の全件数を取得
+            name = JpaConst.Q_EMP_COUNT,
+            query = JpaConst.Q_EMP_COUNT_DEF),
+    @NamedQuery(
+//            指定された社員番号が既にデータベースに存在しているか調べる
+
+            name = JpaConst.Q_EMP_COUNT_REGISTERED_BY_CODE,
+            query = JpaConst.Q_EMP_COUNT_REGISTERED_BY_CODE_DEF),
+    @NamedQuery(
+//            従業員がログインするときに社員番号とパスワードが正しいかチェックするためのモノ
+            name = JpaConst.Q_EMP_GET_BY_CODE_AND_PASS,
+            query = JpaConst.Q_EMP_GET_BY_CODE_AND_PASS_DEF)
+})
+
+@Getter //全てのクラスフィールドについてgetterを自動生成する(Lombok)
+@Setter //全てのクラスフィールドについてsetterを自動生成する(Lombok)
+@NoArgsConstructor //引数なしコンストラクタを自動生成する(Lombok)
+@AllArgsConstructor //全てのクラスフィールドを引数にもつ引数ありコンストラクタを自動生成する(Lombok)
+@Entity
+public class Employee {
+
+    /**
+     * id
+     */
+    @Id
+    @Column(name = JpaConst.EMP_COL_ID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 社員番号
+     */
+    @Column(name = JpaConst.EMP_COL_CODE, nullable = false, unique = true)
+//    ↑のunique=trueは一意制約といい、既に存在している社員番号は登録できない旨をデータベースに指定するための設定
+    private String code;
+        
+    /**
+     * 氏名
+     */
+    @Column(name = JpaConst.EMP_COL_NAME, nullable = false)
+    private String name;
+
+    /**
+     * パスワード
+     */
+    @Column(name = JpaConst.EMP_COL_PASS, length = 64, nullable = false)
+//    パスワードの最大文字数が64文字
+    private String password;
+
+    /**
+     * 管理者権限があるかどうか（一般：0、管理者：1）
+     */
+    @Column(name = JpaConst.EMP_COL_ADMIN_FLAG, nullable = false)
+    private Integer adminFlag;
+
+    /**
+     *登録日時
+     */
+    @Column(name = JpaConst.EMP_COL_CREATED_AT, nullable = false)
+    private LocalDateTime createdAt;
+
+    /**
+     * 更新日時
+     */
+    @Column(name = JpaConst.EMP_COL_UPDATED_AT, nullable = false)
+    private LocalDateTime updatedAt;
+
+    /**
+     * 削除された従業員かどうか（現役：0、削除済み：1）
+     */
+    @Column(name = JpaConst.EMP_COL_DELETE_FLAG, nullable = false)
+    private Integer deleteFlag;
+
+}
